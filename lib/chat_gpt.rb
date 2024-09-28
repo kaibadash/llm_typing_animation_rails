7eb6d8f2b8a1d7554ed323bbc92d773ab393dfcb
kaiba <kaibadash@gmail.com>
2024-09-28 14:06:29 +0900

    Pass index

--- lib/chat_gpt.rb
+++ lib/chat_gpt.rb
@@ -5,8 +5,8 @@ require "openai"
 class ChatGpt
   def self.chat(user_id, message)
     user = User.find(user_id)
-    return dummy_gpt(user, message)
-    #lm_studio(user, message)
+    #return dummy_gpt(user, message)
+    chat_gpt(user, message)
   end
 
   def self.chat_gpt(user, prompt)
@@ -16,12 +16,19 @@ class ChatGpt
     )
     # https://platform.openai.com/docs/models/overview
     # Rails.logger.info(client.models.list.map(&:id))
+    index = 0
     client.chat(
       parameters: {
         model: "gpt-4o-mini",
         messages: [{ role: "user", content: prompt }],
         stream: proc do |chunk, _bytesize|
-          ChatChannel.broadcast_to(user, chunk.dig("choices", 0, "delta", "content"))
+          Rails.logger.info(chunk)
+          ChatChannel.broadcast_to(user, {
+            index: index,
+            status: chunk.dig("choices", 0, "finish_reason").present? ? :finished : :progress,
+            content: chunk.dig("choices", 0, "delta", "content")
+          })
+          index += 1
         end,
       },
     )
@@ -30,10 +37,13 @@ class ChatGpt
   # @deprecated
   def self.dummy_gpt(user, message)
     Rails.logger.info("start send message to #{user.email} #{message}")
+    index = 0
     100.times do
-      "Kaigi on Rails! ".chars.each do |chunk|
-        ChatChannel.broadcast_to(user, chunk)
+      "Kaigi on Rails! ".scan(/.{1,2}/).each do |chunk|
+        ChatChannel.broadcast_to(user, { index: index, status: :progress, content: chunk })
+        index += 1
       end
     end
+    ChatChannel.broadcast_to(user, { index: index, status: :finished })
   end
 end
